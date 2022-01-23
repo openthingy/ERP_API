93fb0e1f9a9ba86c57397750eb559d1d0c0c91f6
danielalexis <danielalexis.o.pereira@gmail.com>
2022-01-23 02:27:16 +0000

    Added new auth system (To be redesigned tbh)

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const createError = require('http-errors');
 const express = require('express');
 const walkSync = require('walk-sync');
-
+const {MongoClient} = require('mongodb');
 const crypto = require('crypto');
 const app = express();
+
+// Configuration File
+const { dirname } = require('path');
+const config = require(dirname(require.main.filename) + '/../config.json'); // ../ because we are in the bin folder
 
 // Logging
 const debug = require('debug');
 const log   = debug('app:log');
-const info  = debug('app:info');
 const warn  = debug('app:warn'); 
 const error = debug('app:error'); 
 
@@ -17,29 +20,57 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(function (req, res, next) {
   req.id = crypto.randomUUID(); // Generate a unique ID for each request
-  log(`${req.id}: IP: ${req.ip} Time: ${new Date()}`); // For Tracing purposes
+  log(`${req.id}: IP: ${req.ip} Path: ${req.path} Time: ${new Date()}`); // For Tracing purposes
   next();
 });
 
-app.use(function (req, res, next){
+app.use(async function (req, res, next){
   // Authentication Middleware
   // This will overwrite any other http error code including 404
+
+  // Checks if a Authorization header is present
+  // If yes it will check if the key is valid
+  // If no it will return a 401 Unauthorized
+  // Checks if theres a Login Header present
+  // This indicates the server to allow the user to access the login route (/account/login)
   log(`${req.id}: Authentication started`);
-  if (req.header('Authorization') == "API-KEY") {
-    log(`${req.id}: Authentication successful`);
-    next();
-  }
-  else if (!req.header("Authorization")) {
-    log(`${req.id}: Authentication failed (no header present)`);
-    next(createError(401)); 
-    return;
-}  else {
-    error(req.id + ": HTTP Auth request failed due to invalid Authorization header (neither missing nor present");
-    next(createError(500));
-    return;
+  const db_url = "mongodb://"+config.database.user+":"+ encodeURIComponent(config.database.password) +"@"+ config.database.host +":"+ config.database.port +"/" + "?ssl=false";
+  log(req.id + " Connecting to database: " + db_url);
+  // Connect to the db
+  const client = new MongoClient(db_url);
+  try {
+    await client.connect();
+    db = await client.db(config.database.database);
+
+    // Check if the user is authenticated
+    if (req.header("Authorization") && !req.header("Login")) {
+      log(`${req.id}: Authorization header found`);
+      // Check if the user session is valid
+      const key = await db.collection("sessions").find({"key": req.header("Authorization")}).count();
+
+      if (key > 0) { 
+        log(`${req.id}: Authentication successful`);
+        next();
+      } else {
+        log(`${req.id}: Authentication failed`);
+        next(createError(401)); // Unauthorized
+      }
+    
+    } else if (req.header("Login") && req.path == "/account/login") { // Verify if the user is trying to login and only allow if the path is /account/login
+      log(`${req.id}: Login attempt`);	
+      next();  
+    } else {
+      log(`${req.id}: Missing Authentication`);
+      next(createError(401)); // Unauthorized
+    }
+  } catch (err) {
+    error("An error ocurred during MongoDB connection/query: " + err);
+    next(createError(500)); // Internal Server Error
+  } finally {
+    await client.close();
+    log(req.id + ": Database Connection closed");
   }
 
-  
 });
 
 
@@ -52,11 +83,11 @@ paths.forEach(function (value, index, array) {
     value_index = value.slice(0, -8);
     app.use("/" + value_index, require(dir + value_nojs)); // Route: /test
     app.use("/" + value_nojs, require(dir + value_nojs));  // Route: /test/index
-    info("Added Route: /" + value_index);
-    info("Added Route: /" + value_nojs);
+    log("Added Route: /" + value_index);
+    log("Added Route: /" + value_nojs);
   } else {
     app.use("/" + value_nojs, require(dir + value_nojs));
-    info("Added Route: /" + value_nojs);
+    log("Added Route: /" + value_nojs);
   }
 })
 
@@ -72,10 +103,6 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  //res.locals.message = err.message;                                     //Not sure what does this do
-  //res.locals.error = req.app.get('env') === 'development' ? err : {};   // This requires a view engine maybe?
-
   // render the error page
   res.status(err.status || 500);
   res.json({"code": err.status, "error.message":err.message});
