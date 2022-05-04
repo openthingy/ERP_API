fd27393db1012bce78ecbcf7de7a3c61479cb545
danielalexis <danielalexis.o.pereira@gmail.com>
2022-05-05 01:05:21 +0200

    fix bugs

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,6 +6,7 @@ else
   exit
 fi
 
+clear
 echo "
 ------------------------------------------
                GesEnterprise
@@ -20,7 +21,7 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run as root or sudo"
   exit
 fi
-clear
+
 
 # Get versions codename
 codename=$(lsb_release -cs)
@@ -35,7 +36,7 @@ fi
 
 # Install required packages
 echo "Installing required packages"
-sudo apt install wget gnupg2 ca-certificates lsb-release ubuntu-keyring software-properties-common -y > /dev/null
+sudo apt-get install wget gnupg2 ca-certificates lsb-release ubuntu-keyring software-properties-common -y > /dev/null
 if [ $? -ne 0 ]; then
     echo "ERROR: unable to install required packages"
     exit
@@ -43,8 +44,8 @@ fi
 
 # NginX (Engine X, that's how its pronounced)
 echo "Installing NginX"
-curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo gpg --dearmor | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg > /dev/null
-echo deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/mainline/ubuntu $codename nginx | sudo tee /etc/apt/sources.list.d/nginx-mainline.list
+curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo gpg --dearmor > /usr/share/keyrings/nginx-archive-keyring.gpg > /dev/null
+echo deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/mainline/ubuntu $codename nginx > /etc/apt/sources.list.d/nginx-mainline.list
 echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" | sudo tee /etc/apt/preferences.d/99nginx > /dev/null
 sudo apt-get update > /dev/null
 sudo apt-get install nginx -y > /dev/null
@@ -53,14 +54,14 @@ if [ $? -ne 0 ]; then
     exit
 fi
 echo "Starting NginX"
-sudo systemctl daemon-reload
-sudo systemctl start nginx
-sudo systemctl enable nginx
+sudo systemctl daemon-reload > /dev/null
+sudo systemctl start nginx > /dev/null
+sudo systemctl enable nginx > /dev/null
 
 # NodeJs (latest LTS version, if i remember to change this to the latest LTS version)
 nodeVersion=16
 curl -fsSL https://deb.nodesource.com/setup_$nodeVersion.x | sudo -E bash - > /dev/null
-echo -e "Package: *\nPin: origin deb.nodesource.com\nPin-Priority: 900\n" | sudo tee /etc/apt/preferences.d/99nodesource > /dev/null
+echo -e "Package: *\nPin: origin deb.nodesource.com\nPin-Priority: 900\n" > /etc/apt/preferences.d/99nodesource
 echo "Installing NodeJS $nodeVersion"
 sudo apt-get update > /dev/null
 sudo apt-get install -y nodejs > /dev/null
@@ -70,8 +71,8 @@ if [ $? -ne 0 ]; then
 fi
 
 # MongoDB
-curl -fsSL https://www.mongodb.org/static/pgp/server-5.0.asc | sudo gpg --dearmor | sudo tee /usr/share/keyrings/mongodb-server-keyring.gpg > /dev/null
-echo deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-keyring.gpg] https://repo.mongodb.org/apt/ubuntu $codename/mongodb-org/5.0 multiverse | sudo tee /etc/apt/sources.list.d/mongodb-server.list
+curl -fsSL https://www.mongodb.org/static/pgp/server-5.0.asc | sudo gpg --dearmor > /usr/share/keyrings/mongodb-server-keyring.gpg > /dev/null
+echo deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-keyring.gpg] https://repo.mongodb.org/apt/ubuntu $codename/mongodb-org/5.0 multiverse > /etc/apt/sources.list.d/mongodb-server.list
 echo "Installing MongoDB"
 sudo apt-get update > /dev/null
 sudo apt-get install -y mongodb-org > /dev/null
@@ -81,6 +82,6 @@ if [ $? -ne 0 ]; then
 fi
 echo "Warning: MongoDB is not secured by default"
 echo "Starting MongoDB"
-sudo systemctl daemon-reload
-sudo systemctl start mongod
-sudo systemctl enable mongod
+sudo systemctl daemon-reload > /dev/null
+sudo systemctl start mongod > /dev/null
+sudo systemctl enable mongod > /dev/null
