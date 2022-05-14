e54a168047fb6bd9c0a952a5d1fd45e6374cd378
danielalexis <danielalexis.o.pereira@gmail.com>
2022-05-14 19:22:50 +0100

    enforce code style

--- routes/test/index.js
+++ routes/test/index.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const gesenterprise = require("gesenterprise");
 
 
 // This is a example route
 /* GET users listing. */
-router.all('/', async function(req, res, next) {
+router.all("/", async function(req, res) {
   // All responses must be sent in json
   const key = await gesenterprise.auth.login("teste@gmail.com", "teste");
   res.json({"hello": "world", "key": key});
