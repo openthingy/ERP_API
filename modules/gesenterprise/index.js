97f0616cbf8b1ea08d50b7703f0635c3a1a2e561
danielalexis <danielalexis.o.pereira@gmail.com>
2022-05-25 23:29:31 +0100

    update methods aswell improve comments

--- modules/gesenterprise/index.js
+++ modules/gesenterprise/index.js
@@ -1,23 +1,51 @@
 const config = require("./config.json");
+const mongoUrl = config.dbCredentials.mongoUrl;
+const mongoDb = config.dbCredentials.database;
 const debug  = require("debug")("debug");
 const mongo = require("mongodb");
-const crypto = require("crypto");
+//const crypto = require("crypto");
 // Logging
 const info = debug.extend("info");
 const warn = debug.extend("warn");
 const error = debug.extend("error");
-info.color = 4;
-warn.color = 3;
-error.color = 1;
+info.color = 4; //Blue
+warn.color = 3; // Yellow
+error.color = 1; // Red
 
+
+function sanitizeInput(input) {
+  // Sanitize input to prevent XSS and NoSQL injection
+  //return input.replace(/[^a-zA-Z0-9]/g, "");
+  // TO BE COMPLETED
+  return input;
+
+}
+
+/**
+* Database related stuff, like get user data,etc...
+* @class
+*/
 class db {
-  static async userFromKey(key) {
-    const client = new mongo.MongoClient(config.database.mongo_url);
+  /**
+  * Gets user data from Session Id.
+  * @function
+  * @param sessionId Session Id stored on the database.
+  * @returns {object} Returns whether session is valid, if so also returns user ObjectId
+  */
+  static async isSessionValid(sessionId) {
+    sessionId = sanitizeInput(sessionId);
+    sessionId = mongo.ObjectId(sessionId);
+    const client = new mongo.MongoClient(mongoUrl);
     try {
       await client.connect();
-      const db_client = await client.db(config.database.db);
-      const key_user = await db_client.collection("api").findOne({"key": key});
-      return key_user.emp_id;
+      const dbClient = await client.db(mongoDb);
+      const session = await dbClient.collection("sessions").findOne({"sessionId": sessionId});
+      if (session.count() > 0 ) { // Add time validation
+        return {
+          "session": true,
+          "userId": session.empId.toString()
+        };
+      } else { return { "session": false }; }
     } catch (err) {
       error(err);
       return false;
@@ -25,16 +53,23 @@ class db {
       await client.close();
     }
   }
- 
-  static async emp_info(emp_id) {
-    // emp_id must be a ObjectID()
 
-    const client = new mongo.MongoClient(config.database.mongo_url);
+  /**
+  * Gets user data from Session Id.
+  * @function
+  * @param empId Employee Id stored on the database.
+  * @returns {object} Returns all user data
+  */
+  static async empInfo(empId) {
+    empId = sanitizeInput(empId);
+    empId = mongo.ObjectId(empId);
+    const client = new mongo.MongoClient(mongoUrl);
     try {
       await client.connect();
-      const db_client = await client.db(config.database.db);
-      const emp_info = await db_client.collection("employees").findOne({"_id": emp_id});
-      return emp_info;
+      const dbClient = await client.db(mongoDb);
+      const empData = await dbClient.collection("employees").findOne({"_id": empId}, {"_id": 0});
+      if (empData.count() > 0) { return empData; } 
+      else { return false; }
     } catch (err) {
       error(err);
       return false;
@@ -43,70 +78,13 @@ class db {
     }
   }
 }
-  
-class auth {
-  static async is_key_valid(key) {
-    const client = new mongo.MongoClient(config.database.mongo_url);
-    try {
-      await client.connect();
-      const db_client = await client.db(config.database.db);
-      const is_valid = await db_client.collection("api").findOne({"key": key}).count();
-      return is_valid; // If it finds it will return 1 (true) else it will return 0 (false)
-    } catch (err) {
-      error(err);
-      return false;
-    } finally {
-      await client.close();
-    }
-  }
 
-  static async login(email, password) {
-    // Password must be a SHA-256 encrypted string
-    const client = new mongo.MongoClient(config.database.mongo_url);
-    try {
-      await client.connect();
-      const db_client = await client.db(config.database.db);
-      const emp_info = await db_client.collection("employees").findOne({"email": email, "password": password});
-      if (!emp_info) {return false;}
-      
-      // generate API key
-      const key = crypto.randomBytes(32).toString("hex");
-      await db_client.collection("api").insertOne({
-        "key": key,
-        "emp_id": emp_info._id,
-        "created_at": new Date()
-      });
-      return key;
-    } catch (err) {
-      error(err);
-      return false;
-    } finally {
-      await client.close();
-    }
-  }
-
-  static async logout(key) {
-    const client = new mongo.MongoClient(config.database.mongo_url);
-    try {
-      await client.connect();
-      const db_client = await client.db(config.database.db);
-      const deleted = await db_client.collection("api").deleteOne({"key": key});
-      if (deleted.deletedCount == 0) { return false; }
-      return true;
-    } catch (err) {
-      error(err);
-      return false;
-    } finally {
-      await client.close();
-    }
-  }
-}
 
 module.exports = {
   info,
   warn,
   error,
   config,
-  db,
-  auth
+  sanitizeInput,
+  db
 };
